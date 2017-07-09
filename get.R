56335b20895b5c07683abe6e43bebd7ce816c69b
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-09 11:40:11 +0200

    error handling

--- get.R
+++ get.R
@@ -10,13 +10,21 @@
 #' @details Queryies ichanhazip.com through SSL for your public Ip v4.
 #' 
 #' @export
-public <- function(throw=TRUE) {
+publicV4 <- function(throw=TRUE) {
   stopifnot(is.logical(throw))
   res <- curl::curl_fetch_memory('https://icanhazip.com', 
                                  curl::new_handle())
-  publicIp <- sub('\n$', '', rawToChar(res$content))
-  if (throw && length(publicIp) != 1L) stop('oops...')
-  return(if (length(publicIp)) publicIp else NULL)
+  publicIp <- trimws(rawToChar(res$content))
+  if (length(publicIp) == 1L && 
+      grepl('^(\\d+\\.?)+$', publicIp)) {
+    return(publicIp)
+  } else if (throw && 
+             length(publicIp) != 1L || 
+             !grepl('^(\\d+\\.?)+$', publicIp)) {
+    stop('oops...')
+  } else {
+    return(NULL)
+  }
 }
 
 #' Get your private IP address as v4
@@ -30,16 +38,45 @@ public <- function(throw=TRUE) {
 #' other OS.
 #' 
 #' @export
-private <- function(throw=TRUE) {
+privateV4 <- function(throw=TRUE) {
   stopifnot(is.logical(throw))
-  if(grepl('win', .Platform$OS.type, TRUE)) {
+  if (grepl('win', .Platform$OS.type, TRUE)) {
     cmdout <- system2('cmd.exe', input='ipconfig | findstr /i ipv4', 
                       stdout=TRUE, stderr=TRUE)
     ipline <- grep('ipv4\\s+address', cmdout, ignore.case=TRUE, value=TRUE)
-    privateIp <- regmatches(ipline, regexpr('(\\d+\\.{0,1})+\\s*$', ipline))
-    if (throw && length(privateIp) != 1L) stop('oops...')
-    return(if (length(privateIp)) privateIp else NULL)
+    privateIp <- regmatches(ipline, regexpr('(\\d+\\.?)+\\s*$', ipline))
+    if (length(privateIp) == 1L && 
+        grepl('^(\\d+\\.?)+$', privateIp)) {
+      return(privateIp)
+    } else if (throw && 
+               length(privateIp) != 1L || 
+               !grepl('^(\\d+\\.?)+$', privateIp)) {
+      stop('oops...')
+    } else {
+      return(NULL)
+    }
   } else {
-    
+    #..#
+    NULL
+  }
+}
+
+#' Get a list of your private and public IPv4
+#' 
+#' @param throw Logical. In case your Ip addresses could not be resolved 
+#' should an error be thrown?
+#' @return Named list; if \code{!throw} \code{NULL} on failure.
+#' 
+#' @export
+listV4 <- function(throw=TRUE) {
+  stopifnot(is.logical(throw))
+  IPS <- list(public=publicV4(throw=throw), private=privateV4(throw=throw))
+  if (length(IPS) == 2L && 
+      all(grepl('^(\\d+\\.?)+$', IPS))) {
+    return(IPS)
+  } else if (throw) {
+    stop('oops...')
+  } else {
+    return(NULL)
   }
 }
